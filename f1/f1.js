e38223ebefcb8edb05455cee6c7f16f0799dd667
meggyedes <soosdanielmarcel@gmail.com>
2024-10-08 14:16:50 +0200

    Update f1.js

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,11 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 async function f1 (lat, long, timeStr) {
-    //TODO
+    const response = await fetch(APIAdress + latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m);
+    const data = await response.json();
+    const max = data.daily.temperature_2m_max[0];
+    const min = data.daily.temperature_2m_min[0];
+    const avgTemp = Math.floor((max + min) / 2);
 }
 
 module.exports = f1;
